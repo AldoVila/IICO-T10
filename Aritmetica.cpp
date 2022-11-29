11331fd63c2575b0e75390b0f33e4baea2d10b19
Aldo Martinez <aldomartinez26@hotmail.com>
2022-11-28 18:15:10 -0600

    Ultima version de solo texto

--- Aritmetica.cpp
+++ Aritmetica.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 int main ()
-{	printf("Hola, este programa realizara funciones especificas,\n de acuerdo a lo que se le solicite.\n");
-	printf("\nPresione el numero de lo que desea que ocurra.");
+{	int Opcion; 
+	int NS1, NS2, NS3;
+	int NR1, NR2, NR3;
+	int NM1, NM2, NM3;
+	int ND1,ND2, ND3;
+
+	printf("Hola, este programa realizara funciones especificas,\n de acuerdo a lo que se le solicite.\n");
+	printf("\nEl numero 1 para realizar sumas de 2 digitos.\n");
+	printf("\nEl numero 2 para realizar restas de 2 digitos.\n");
+	printf("\nEl numero 3 para realizar multiplicaciones de 2 digitos.\n"),
+	printf("\nEl numero 4 para realizar divisiones de 2 digitos.\n");
+	printf("\nEl numero de lo que desea que ocurra.\n");
+	printf("\n\n");
+	scanf("%i", Opcion);
+	printf("%i",Opcion);
 return 0;
 }
