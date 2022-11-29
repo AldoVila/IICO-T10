0e13f142d6ee20abe94fe8a0c23292d89597a1e7
Aldo Martinez <aldomartinez26@hotmail.com>
2022-11-28 19:28:27 -0600

    Version Final

--- Aritmetica.cpp
+++ Aritmetica.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main ()
 {	int Opcion; 
-	int NS1, NS2, NS3;
-	int NR1, NR2, NR3;
-	int NM1, NM2, NM3;
-	int ND1,ND2, ND3;
+	int NS1, NS2;
+	int NR1, NR2;
+	int NM1, NM2;
+	int ND1,ND2;
+	float NS3, NR3, NM3, ND3;
 
 	printf("Hola, este programa realizara funciones especificas,\n de acuerdo a lo que se le solicite.\n");
 	printf("\nEl numero 1 para realizar sumas de 2 digitos.\n");
@@ -15,7 +16,7 @@ int main ()
 	printf("\n\n");
 	scanf("%i", &Opcion);
 	
-	if(Opcion=1)
+	if(Opcion<=1)
 	 {
 	 printf("\nSuma\n");
 	 printf("Introduza el primer numero: ");
@@ -23,11 +24,11 @@ int main ()
 	 printf("Introduza el segundo numero: ");
 	 scanf("%i",&NS2);
 	 NS3=NS1+NS2;
-	 printf("El resultado es:%i",NS3);
+	 printf("El resultado es:%.2f",NS3);
 	 }
 	else
 	{
-		if(Opcion=2)
+		if(Opcion<=2)
 	 	{
 		printf("\nResta\n");
 	 	printf("Introduza el primer numero: ");
@@ -35,11 +36,11 @@ int main ()
 	 	printf("Introduza el segundo numero: ");
 	 	scanf("%i",&NR2);
 	 	NR3=NR1-NR2;
-	 	printf("El resultado es:%i",NR3);
+	 	printf("El resultado es:%.2f",NR3);
 	    }
 	    else
 	{
-		if(Opcion=3)
+		if(Opcion<=3)
 		{
 		printf("\nMultiplicacion\n");
 		printf("Introduza el primer numero: ");
@@ -47,11 +48,11 @@ int main ()
 		printf("Introduza el segundo numero: ");
 		scanf("%i",&NM2);
 		NM3=NM1*NM2;
-		printf("El resultado es:%i",NM3);
+		printf("El resultado es:%.2f",NM3);
 		}
 		else
 	{
-		if(Opcion=4)
+		if(Opcion<=4)
 		{
 		printf("\nDivision\n");
 	 	printf("Introduza el primer numero: ");
@@ -59,7 +60,7 @@ int main ()
 	 	printf("Introduza el segundo numero: ");
 	 	scanf("%i",&ND2);
 	 	ND3=ND1/ND2;
-	 	printf("El resultado es:%i",ND3);
+	 	printf("El resultado es:%.2f",ND3);
 	 	}
 	 	else
 	    printf("\nIngrese un numero valido");
